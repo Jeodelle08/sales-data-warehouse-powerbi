e96b6b1a23348fba6e0cfe658683dc7d605aaf99
Jeodelle08 <medjomwojeodellemarciale@gmail.com>
2026-03-23 08:30:09 +0100

    Create staging tables for sales data in DE and US

--- sql/Create_staging_tables.sql
+++ sql/Create_staging_tables.sql
@@ -1,1 +1,32 @@
 
+-- 01_create_staging_tables.sql
+DROP TABLE IF EXISTS staging_sales_de;
+DROP TABLE IF EXISTS staging_sales_us;
+
+CREATE TABLE staging_sales_de (
+    order_number INT,
+    order_item INT,
+    sales_date TEXT,
+    customer TEXT,
+    product TEXT,
+    sales_quantity INT,
+    unit_of_measure TEXT,
+    revenue TEXT,
+    currency TEXT,
+    discount TEXT,
+    costs_usd TEXT
+);
+
+CREATE TABLE staging_sales_us (
+    order_number INT,
+    order_item INT,
+    sales_date TEXT,
+    customer TEXT,
+    product TEXT,
+    sales_quantity INT,
+    unit_of_measure TEXT,
+    revenue TEXT,
+    currency TEXT,
+    discount TEXT,
+    costs_usd TEXT
+);
